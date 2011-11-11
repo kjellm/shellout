891804f8398dd347746f43513850bdae26787d2a
Kjell-Magne Øierud <kjellm@acm.org>
2011-11-11 21:15:56 +0100

    Provide some shortcuts

--- lib/shellout.rb
+++ lib/shellout.rb
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 
 # Unicode box drawing characters and block elements
 #
@@ -18,10 +18,27 @@
 # U+258x ▀ ▁ ▂ ▃ ▄ ▅ ▆ ▇ █ ▉ ▊ ▋ ▌ ▍ ▎ ▏
 # U+259x ▐ ░ ▒ ▓ ▔ ▕ ▖ ▗ ▘ ▙ ▚ ▛ ▜ ▝ ▞ ▟
 
+require 'shellout/calendar'
+require 'shellout/shadowbox'
+require 'shellout/table'
+
+# You can mixin this module to get shortcuts to the various functionality
+# provided by the classes below this namespace
 module Shellout
 
-  autoload :Calendar,  'shellout/calendar'
-  autoload :Shadowbox, 'shellout/shadowbox'
-  autoload :Table,     'shellout/table'
+  # See doc for Shellout::Calendar.new
+  def Calendar(*whatever)
+    Shellout::Calendar.new(*whatever)
+  end
+  
+  # See doc for Shellout::Shadowbox.new
+  def Shadowbox(*whatever)
+    Shellout::Shadowbox.new(*whatever)
+  end
+  
+  # See doc for Shellout::Table.new
+  def Table(*whatever)
+    Shellout::Table.new(*whatever)
+  end
   
 end
